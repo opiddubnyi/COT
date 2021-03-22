75129852cc643cdd100decc9f263361cffb58632
Oleksandr Piddubnyi <podd.alexandr@gmail.com>
2021-03-21 17:26:55 -0700

    fixed broken commit.

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+"""
+README:
+All preparation for files needs to be done in according with first_step.py
+functions.
+Those will download data, unzip update and merge in into one file
+
+Here we will define functions to work with DB.
+
+
+
 
 
 """
